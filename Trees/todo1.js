932422325e36e084e0fd40b8ea7e17096c8a70fc
Erin Winking <ewink@yahoo.com>
2021-03-06 23:48:52 -0800

    Trees To Do 1 partailly done.

--- Trees/todo1.js
+++ Trees/todo1.js
@@ -9,12 +9,98 @@ class BST {
     constructor() {
         this.root = null;
     }
-    // add methods here...
+
+    add(val) {
+        let newNode = new BTNode(val)
+        if (this.root) {
+            let currentNode = this.root;
+            while (currentNode) {
+                if (newNode.val < currentNode.val) {
+                    if (currentNode.left) {
+                        currentNode = currentNode.left;
+                    } else {
+                        currentNode.left = newNode;
+                        currentNode = null;
+                    }
+                }   
+                else if (newNode.val > currentNode.val) {
+                    if (currentNode.right) {
+                        currentNode = currentNode.right;
+                    } 
+                    else {
+                        currentNode.right = newNode;
+                        currentNode = null;
+                }
+            }
+                else {
+                    newNode.left = currentNode.left;
+                    newNode.right = currentNode.right;
+                    currentNode.left = newNode;
+                    currentNode.right = null;
+                }
+            }
+        }
+        else {  
+            this.root = newNode;    
+        }
+        return this.root;
+    }
+
+    contains(val) {
+        if (this.root) {
+            let currentNode = this.root;
+            while (currentNode) {
+                if (val === currentNode.val) {
+                    currentNode = null;
+                    console.log('FOUND IT!!')
+                    return true
+                }
+                else if (val < currentNode.val ) {
+                    if (currentNode.left) {
+                        currentNode = currentNode.left;
+                    } else {
+                        currentNode = null;
+                        return false
+                    }
+                }
+                else if (val > currentNode.val ) {
+                    if (currentNode.right) {
+                        currentNode = currentNode.right;
+                    } else {
+                        currentNode = null;
+                        return false
+                    }
+                }
+            }
+        }
+        else {
+            console.log('Nothing in this Tree!')
+            return 0
+        }
+    }
+
 }
-
-let nodeA = 
-
+// let nodeF = new BTNode(12)
+// let nodeA = new BTNode(22), nodeB = new BTNode(5), nodeC = new BTNode(44), nodeD = new BTNode(7), nodeE = new BTNode(18) 
+// let nodeG = new BTNode(10), nodeH = new BTNode(2), nodeI = new BTNode(31), nodeJ = new BTNode(27), nodeK = new BTNode(6)
 
 myBST = new BST
 
-[2,5,6,7,10,12,18,22,27,31,44,]
+myBST.add(12)
+myBST.add(22)
+myBST.add(5)
+myBST.add(44)
+myBST.add(7)
+myBST.add(18)
+myBST.add(10)
+myBST.add(2)
+myBST.add(31)
+myBST.add(27)
+myBST.add(6)
+
+console.log('------ADD------')
+console.log(myBST)
+console.log('------CONTAINS------')
+console.log(myBST.contains(30))
+console.log(myBST.contains(27))
+console.log(myBST.contains(100))
